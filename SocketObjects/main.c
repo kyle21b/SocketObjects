1fd3ab9dc93c30bdfce834f2619838ce1845f955
kyle21b <kyle2194@gmail.com>
2015-04-10 16:37:27 -0400

    Initial Commit

---
+++ SocketObjects/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SocketObjects
+//
+//  Created by Kyle Bailey on 4/10/15.
+//  Copyright (c) 2015 Kyle Bailey. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
